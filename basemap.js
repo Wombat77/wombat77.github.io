68aaf4de97bf5ae5e5b5bd3bf8e9da22008a4830
Roman fendt <roman.fendt@uibk.ac.at>
2018-04-18 16:49:24 +0200

    Plus Karte

--- basemap.js
+++ basemap.js
@@ -1,5 +1,5 @@
 let myMap = L.map("mapdiv");
-let myLayer = L.tileLayer(https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+let myLayer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
 
 myMap.addlayer(myLayer);
 myMap.setView([47.267,11.383],11);
