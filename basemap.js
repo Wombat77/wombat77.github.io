66c389f1e7d56e9b5eaf1391144adc0b5544103f
Roman fendt <roman.fendt@uibk.ac.at>
2018-04-18 16:44:10 +0200

    mit Karte

--- basemap.js
+++ basemap.js
@@ -1,1 +1,5 @@
-alert ("Hallo Welt");
+let myMap = L.map("mapdiv");
+let myLayer = L.tileLayer(https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+
+myMap.addlayer(myLayer);
+myMap.setView([47.267,11.383],11);
